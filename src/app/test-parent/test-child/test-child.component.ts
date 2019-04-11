fd2177c2c7bf2760b9a5e4dc517f073398338c5f
doolri1276 <doolri1277@gmail.com>
2019-04-11 17:27:49 +0900

    - 예제 6-9 @Output() 데코레이터 ( 자식 -> 부모 ), 자식에서 보내는 부분

--- src/app/test-parent/test-child/test-child.component.ts
+++ src/app/test-parent/test-child/test-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-test-child',
@@ -6,8 +6,13 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./test-child.component.css']
 })
 export class TestChildComponent implements OnInit {
+
+  //부모 -> 자식 으로 데이터 전달
   @Input() myAnotherState;
   @Input() clonedVal;
+
+  //자식 -> 부모 로 이벤트 보냄
+  @Output() onChangeChildData = new EventEmitter<number>();
 
   title="Test Child";
 
@@ -16,4 +21,9 @@ export class TestChildComponent implements OnInit {
   ngOnInit() {
   }
 
+  changeMyData() {
+    //비즈니스 로직
+    const resultVal = 8712;
+    this.onChangeChildData.emit(resultVal);
+  }
 }
