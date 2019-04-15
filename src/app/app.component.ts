c2362340a09f68bc03d859c5aac447a6b572aa46
doolri1276 <doolri1277@gmail.com>
2019-04-15 15:44:44 +0900

    - 예제 6-30 ngFor과 템플릿 참조 변수 함께 사용

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,23 @@ import { Component, ElementRef, AfterViewInit, ViewChild, ViewChildren, QueryLis
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit{
-  elemementRef: ElementRef;
+  // elemementRef: ElementRef;
 
   //탐색 결과에 부합하는 첫번째 결과를 바인딩
-  @ViewChild('myInput') myInput: ElementRef;
+  // @ViewChild('myInput') myInput: ElementRef;
   //템플릿에 동일한 이름으로 선언한 템플릿 참조변수를 모두 바인딩하여 담는다.
-  @ViewChildren('myInput') myInputs: QueryList<ElementRef>;
+  @ViewChildren('listItems') itemElems: QueryList<ElementRef>;
 
-  constructor(ef: ElementRef){
-    this.elemementRef = ef;
+  items:any[];
+
+  constructor(){
+    this.items = [
+      {content: 'aaa'},
+      {content: '가나다'}
+    ];
   }
 
   ngAfterViewInit(){
-
+    
   }
 }
